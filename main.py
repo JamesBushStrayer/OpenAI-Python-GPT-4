580c694d8b1987a6fa2ed371c448a67ae1273e52
JamesBushStrayer <139084557+JamesBushStrayer@users.noreply.github.com>
2023-11-06 12:32:13 +0000

    Groups related prompts into a chat conversation

--- main.py
+++ main.py
@@ -4,15 +4,29 @@ import sys
 
 psalms = ("40", "10", "12", "23", "35", "38", "41", "88", "139", "141")
 
+
 def prompts(psalm):
   return (
-  f"Write a 5 to 6 sentence introduction to the spiritual and emotional elements of Psalm {psalm}, and try to connect its meaning and purpose with that of Christian faith.",
-  f"Provide the full text of Psalm {psalm} using the King James Version, formatted for organizational clarity.",
-  f"Create a study guide for Psalm {psalm} that includes both a brief, one-sentence summary of each main point along with the Bible verses they correspond to (be sure to cover every verse in the psalm; do not call the summary “main point”; include the verses being summarized at the end), an extended summary for each main point (do not label the extended summary),  questions that challenge the reader's understanding of each main point (label the challenge questions, “EVALUATE”; omit the colon), and a few questions that ask the reader to reflect on their own life experience for each main point (label the reflection questions, “REFLECT”; omit the colon). Provide answers to the challenge questions, but not the questions for reflection.",
-  f"Describe all the ways Psalm {psalm} embodies or reflects God’s nature in two sections: 1. divine (incommunicable) attributes; and, 2. communicable  attributes. Include biblical references, if applicable.",
-  f"Relate the person and teachings of Jesus Christ and Psalm {psalm}, particularly, as the pertain to the gospel. Include supporting Bible verses for every connection made, especially if there is a match between the words of Jesus and verses in this psalm.",
-  f"List all of the psalms that are identical or highly similar to Psalm {psalm}, whether in part or in whole. Explain the similarities in as much detail as possible."
-)
+    f"Write a 5 to 6 sentence introduction to the spiritual and emotional elements of Psalm {psalm}, and try to connect its meaning and purpose with that of Christian faith.",
+    f"Provide the full text of Psalm {psalm} using the King James Version, formatted for organizational clarity.",
+    f"Create a study guide for Psalm {psalm} that covers the entire psalm with verses being grouped by relationship; precede the study guide with “Study Guide”. Write a concise, one-line header that describes the topical relationship of groups of related verses (set the text style of the header to bold). Next, write a two-part, one paragraph summary that 1) begins with a single topic sentence, which expands on the header and  summarizes the main point presented by the group of verses (set the topic sentence text style to bold), and then reference the Bible book, chapter and verses that comprise the main point (put the reference in parentheses).; and, 2) continuing in the same paragraph, a more detailed summary (5 to 6 sentences in length) that further clarifies the intent and meaning of the main point ((set the text style of the header to regular).  Include the entire psalm (all verses) in the series of main points. After each main point, present one to three questions related to the verses that focus their attention to the most significant aspect(s) of the main point (precede the questions with “Focus”). Follow each question with an answer. Following the questions and answers “Focus” section, ask one to three questions that invite and encourage the reader to reflect on the material and its applicability to their personal experiences, and to relay those experiences by and through their answers to each; precede the questions with “Reflect”. There should be no punctuation after any of these. Do not number anything, but do bullet the reflection questions.  After all the main points are written, write a summary of the main points (or psalm) overall, highlighting their overall (or collective)  meaning and purpose, and connect that meaning and purpose with that of Christian faith and/or divine attributes of God (both communicable and incommunicable), as well as His purpose for man. After each main point, present one to three questions related to the verses that focus their attention to the most significant aspect(s) of the main point (precede the questions with “Focus”). Follow each question with an answer. Following the questions and answers “Focus” section, ask one to three questions that invite and encourage the reader to reflect on the material and its applicability to their personal experiences, and to relay those experiences by and through their answers to each; precede the questions with “Reflect”. There should be no punctuation after any of these. Do not number anything, but do bullet the reflection questions.r answers to each; precede the questions with “Reflect”. There should be no punctuation after any of these. Do not number anything, but do bullet the reflection questions.",
+    f"Describe all the ways Psalm {psalm} embodies or reflects God’s nature in two sections: 1. divine (incommunicable) attributes; and, 2. communicable  attributes. Include biblical references, if applicable.",
+    f"Relate the person and teachings of Jesus Christ and Psalm {psalm}, particularly, as the pertain to the gospel. Include supporting Bible verses for every connection made, especially if there is a match between the words of Jesus and verses in this psalm.",
+    f"List all of the psalms that are identical or highly similar to Psalm {psalm}, whether in part or in whole. Explain the similarities in as much detail as possible."
+  )
+
+
+def messages_arr(prompts_func, psalms_arr):
+  messages_dict_arr = [{
+    "role":
+    "system",
+    "content":
+    "You are creating study material for parishoners participating in a Bible study group or class, and your task is to generate theological discourse on a variety of Psalms per the specifications provided by me."
+  }]
+  for psalm in psalms_arr:
+    for prompt in prompts_func(psalm):
+      messages_dict_arr.append({"role": "user", "content": prompt})
+  return messages_dict_arr
 
 
 try:
@@ -33,26 +47,9 @@ except KeyError:
   """)
   exit(1)
 
-for psalm in psalms:
-  filename = f"Psalm_{psalm}.txt"
-  if os.path.isfile(filename):
-    print(f"File Psalm_{psalm}.txt exists — skipping\n")
-  else:
-    print(f"Writing Psalm_{psalm}.txt...\n")
-    
-    for prompt in prompts(psalm):
-      response = openai.ChatCompletion.create(
-      model="gpt-4",
-      messages=[{"role": "user", "content": prompt}],
-      temperature=1,
-      top_p=1,
-      n=3
-    )
-      print(str(f"User: {prompt}") +
-    '\n\n')
-      print(
-      str(f"AI: {response.choices[0].message['content']}") +
-      '\n\n-------------------------\n\n')
-      with open(filename, "a") as file:
-        file.write(str(response.choices[0].message.content) +
-        '\n\n-------------------------\n\n')
+response = openai.ChatCompletion.create(model="gpt-4",
+                                        messages=messages_arr(prompts, psalms),
+                                        temperature=1,
+                                        top_p=1,
+                                        n=3)
+print(response)
